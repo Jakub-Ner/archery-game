806ef6bed5558a7bf54e5ad3420a1b092f683616
JingleBellczyk <agabela121@gmail.com>
2025-03-27 20:27:13 +0100

    dodane import react

--- frontend-service/src/App.tsx
+++ frontend-service/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import React from "react";
 
 function App() {
   return (
