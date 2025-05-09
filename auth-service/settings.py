39bd80dcd631d124e4fa2e2038d6cba4460df6f4
abelczyk <agata.belczyk@capgemini.com>
2025-05-09 17:38:41 +0200

    added auth url const & port gamedb set to 5432

--- auth-service/settings.py
+++ auth-service/settings.py
@@ -3,4 +3,5 @@ import os
 USER = 'postgres' if os.getenv('DB_HOST') is None else os.getenv('DB_HOST')
 PASSWORD = 'postgres' if os.getenv('DB_PASSWORD') is None else os.getenv('DB_PASSWORD')
 DB_NAME = 'gamedb' if os.getenv('DB_NAME') is None else os.getenv('DB_NAME')
+DB_URL = 'localhost:5432' if os.getenv('DB_URL') is None else os.getenv('DB_URL')
 """  """
