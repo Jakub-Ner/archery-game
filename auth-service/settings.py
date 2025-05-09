6f471209af5ab0d0226a7019cc42344cfe9bcb10
abelczyk <agata.belczyk@capgemini.com>
2025-05-09 17:27:25 +0200

    added fixes to database schema in auth&player services, python libs management

--- auth-service/settings.py
+++ auth-service/settings.py
@@ -1,5 +1,6 @@
 import os
 
 USER = 'postgres' if os.getenv('DB_HOST') is None else os.getenv('DB_HOST')
-PASSWORD = '123' if os.getenv('DB_PASSWORD') is None else os.getenv('DB_PASSWORD')
-DB_NAME = 'postgres' if os.getenv('DB_NAME') is None else os.getenv('DB_NAME')
+PASSWORD = 'postgres' if os.getenv('DB_PASSWORD') is None else os.getenv('DB_PASSWORD')
+DB_NAME = 'gamedb' if os.getenv('DB_NAME') is None else os.getenv('DB_NAME')
+"""  """
